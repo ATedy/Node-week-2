8d6b45e7b9457619869f2bf8ff620eab15b757b3
Amanuel <weditedi25@gmail.com>
2021-04-24 15:56:43 +0100

    get, post, create working

--- server.js
+++ server.js
@@ -1,28 +1,71 @@
-const albumsData = [
-  {
-    albumId: "10",
-    artistName: "Beyoncé",
-    collectionName: "Lemonade",
-    artworkUrl100:
-      "http://is1.mzstatic.com/image/thumb/Music20/v4/23/c1/9e/23c19e53-783f-ae47-7212-03cc9998bd84/source/100x100bb.jpg",
-    releaseDate: "2016-04-25T07:00:00Z",
-    primaryGenreName: "Pop",
-    url:
-      "https://www.youtube.com/embed/PeonBmeFR8o?rel=0&amp;controls=0&amp;showinfo=0",
-  },
-  {
-    albumId: "11",
-    artistName: "Beyoncé",
-    collectionName: "Dangerously In Love",
-    artworkUrl100:
-      "http://is1.mzstatic.com/image/thumb/Music/v4/18/93/6d/18936d85-8f6b-7597-87ef-62c4c5211298/source/100x100bb.jpg",
-    releaseDate: "2003-06-24T07:00:00Z",
-    primaryGenreName: "Pop",
-    url:
-      "https://www.youtube.com/embed/ViwtNLUqkMY?rel=0&amp;controls=0&amp;showinfo=0",
-  },
-];
+const express = require("express");
+const albumsData = require("./Data");
+
+const app = express();
+
+// this should be always before our route code
+// content-Type:application/json
+// middleware module
+app.use(express.json());
 
 app.get("/albums", function (req, res) {
   res.send(albumsData);
 });
+
+// searching using the id param, getting data
+app.get("/albums/:id", function (req, res) {
+  let id = req.params.id;
+  console.log(albumsData);
+
+  let filteredAlbum = albumsData.find(
+    (singleData) => singleData.albumId === id
+  );
+
+  if (!filteredAlbum) {
+    res.sendStatus(404);
+  }
+  res.send(filteredAlbum);
+});
+
+// post method, adding new element
+app.post("/albums", function (req, res) {
+  let newAlbum = req.body;
+
+  if (!newAlbum.albumId) {
+    res.status(400);
+    res.send("Album Id required");
+  } else if (albumsData.find((album) => album.albumId === newAlbum.albumId)) {
+    res.status(400);
+    res.send("Album already exists");
+  } else {
+    albumsData.push(newAlbum);
+    res.status(201);
+    console.log(newAlbum);
+    res.send(newAlbum);
+  }
+});
+
+// deleting an element
+app.delete("/albums/:albumId", (req, res) => {
+  let id = req.params.albumId;
+  let deletedAlbumIndex = albumsData.findIndex((album) => album.albumId === id);
+  if (deletedAlbumIndex > -1) {
+    albumsData.slice(deletedAlbumIndex, 1);
+    res.status(204);
+    res.send("Album Successfully deleted");
+    res.end();
+  }
+});
+
+// updating/put
+app.put("/albums/:albumId", (req, res) => {
+  let newUpdatedAlbum = req.body;
+  let updatedAlbumIndex = albumsData.findIndex(
+    (album) => album.albumId === req.params.albumId
+  );
+
+  albumsData[updatedAlbumIndex] = newUpdatedAlbum;
+  res.send(newUpdatedAlbum);
+});
+
+app.listen(3000, () => console.log(`listening on 3000`));
